3a4a58f7b6dda8aaed5a6f6cb941871301b32721
hifa <huzaifa200418@gmail.com>
2023-10-31 06:18:57 +0500

    Compatiable with Win

--- b_additive.py
+++ b_additive.py
@@ -2,35 +2,44 @@
 import a_baseDic as bDic
 import sys
 
-def decrypt(message,i):
+
+def decrypt(message, i):
     i = int(i)
     message = ''.join(message.split()).lower()
     plainText = ""
     for char in message:
-        char = ((ord(char) - 96)%26 + int(i))%26
+        char = ((ord(char) - 96) % 26 + int(i)) % 26
         plainText += bDic.dic[char]
-    plainText = ' '.join([plainText[i:i+5] for i in range(0, len(plainText), 5)])
-    print(f"{plainText}_{26-i}_{i}")
-    return plainText    
+    plainText = ' '.join([plainText[i:i + 5] for i in range(0, len(plainText), 5)])
+    print(f"{plainText}_{26 - i}_{i}")
+    return plainText
 
-def encrypt(message,i):
+
+def encrypt(message, i):
     message = ''.join(message.split()).lower()
-    cipherText = "" 
+    cipherText = ""
     for char in message:
-        char = ((ord(char)-96) % 26 + int(i))%26
+        char = ((ord(char) - 96) % 26 + int(i)) % 26
         cipherText += bDic.dic[char]
-    cipherText = ' '.join([cipherText[i:i+5] for i in range(0, len(cipherText), 5)])
+    cipherText = ' '.join([cipherText[i:i + 5] for i in range(0, len(cipherText), 5)])
     print(cipherText)
-    return cipherText   
+    return cipherText
 
-#1_additive.py encrypt message index
+
+# 1_additive.py encrypt message index
 message = sys.argv[2]
 if sys.argv[1] == "encrypt":
     index = sys.argv[3]
-    encrypt(message,index)
+    encrypt(message, index)
 elif sys.argv[3] != '0':
     index = sys.argv[3]
-    decrypt(message,index)
+    print(f"{encrypt(message, i)}_{26 - i}_{i}")
 else:
-    for i in range(1,26):
-        decrypt(message,i)
+    for i in range(1, 26):
+        print(f"{encrypt(message, i)}_{26 - i}_{i}")
+
+# ./b_additive.py encrypt "Message goes here" 2
+# ./b_additive.py decrypt "Message goes here" 24
+
+# For windows
+# python.exe b_additive.py encrypt "just a test" 2
