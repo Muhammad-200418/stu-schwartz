b35fc151962c52c43e878f4100d9a7216556a18d
hifa <huzaifa200418@gmail.com>
2023-10-31 18:29:03 +0500

    Ready for the main, tests remain

--- c1_inverse.py
+++ c1_inverse.py
@@ -1,28 +1,25 @@
 #!/bin/python3
-import sys
+import sys, a_baseDic as bDic
 
 if len(sys.argv) < 2:
     print('''Please enter the number to calculate the inverse. 
 Multiples of 2 and 13 are not allowed.
     
-
-Windows
+- Windows
 python.exe c1_inverse.py <Num>
-Linux
+- Linux
 ./c1_inverse.py <Num>''')
     sys.exit(0)
 
+
+def calcinverse(num):
+    if num % 2 == 1 and num % 13 != 0:  # Multiples of 2 and 13 don't have an inverse
+        key = num % 26
+        inverse = bDic.inverse[key]
+        return inverse
+    else:
+        print("Please enter a valid number.")
+
+
 num = int(sys.argv[1])
-if num % 2 == 1 and num % 13 != 0:  # Multiples of 2 and 13 don't have an inverse
-    for i in range(1, num + 1, 2):
-        if i % 13 != 0:  # Only go for values that aren't multiples of 2 or 13
-            found = False
-            counter = 1
-            while not found:
-                if (i * counter) % 26 == 1:
-                    found = True
-                    print(f"Inverse of {i} is {counter}")
-                else:
-                    counter += 1
-else:
-    print("Please enter a valid number")
+print(f"The inverse of {num} is {calcinverse(num)}")
