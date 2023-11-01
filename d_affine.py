4058f2adab6169b792c81f9e001f4d696f7fd9ae
hifa <huzaifa200418@gmail.com>
2023-11-01 10:57:45 +0500

    completed the backbone and overall flow

--- d_affine.py
+++ d_affine.py
@@ -1,85 +1,114 @@
 #!/bin/python3
 
-#Take a message.
-#Encipher or decipher it using affine.
-#Verify the keys.
-#Because the key unknown,try every possible key. 
-#Linguistic approach
-import sys,math,a_baseDic as bDic
+# Take a message.
+# Encipher or decipher it using affine.
+# Verify the keys.
+# Because the key unknown,try every possible key.
+# Linguistic approach
+import sys, math, a_baseDic as bDic,time
 
-def verify(key):  #Checking if the number is co prime
-	if math.gcd(key,26) == 1 and key != 1:
-		return True
-	else:
-		return False
 
-def encrypt(a,b,message):
-	if verify(a):
-		message = ''.join(message.split()).lower()
-		cipherText = "" 
-		b = int(b) % 26
-		for char in message:
-			char = ((ord(char) -96) % 26)
-			char = ((char * a) + b) % 26
-			cipherText += bDic.dic[char]
-		cipherText = ' '.join([cipherText[i:i+5] for i in range(0, len(cipherText), 5)])
-		print(cipherText)
-		return cipherText 
-	else:
-		print(f"{a} is not a valid key")
+# Verifying that the multiplication key is correct
+def verify(key):  # Checking if the number is co prime
+    if math.gcd(key, 26) == 1 and key != 1:
+        return True
+    else:
+        return False
 
-def decrypt(a,b,message):
-	encrypt(a,b,message)
 
-def reversal(a,b):
-	c = bDic.inverse[a]
-	d = ((26-b) * c) % 26
-	return c,d
+def encrypt(a, b, text):
+    if verify(a):
+        text = ''.join(text.split()).lower()
+        output = ""
+        b = int(b) % 26
+        for char in text:
+            char = ((ord(char) - 96) % 26)
+            char = ((char * a) + b) % 26
+            output += bDic.dic[char]
+        output = ' '.join([output[i:i + 5] for i in range(0, len(output), 5)])
+        return output
+    else:
+        print(f"{a} is not a valid key")
 
-#If we know what was the original key, we can find it out using reversal. However, otherwise, we have to brute force the key. 
-#To brute force, all we need is magnitude of repitition of characters, and boom. 
-def countChars(message):
-	counts = {}
-	message = ''.join(message.split()).lower() 
-	for i in message:
-		if i in counts:
-			counts[i] += 1
-		else:
-			counts[i] = 1
-	return(counts)
 
-def bruteForce(cipher,expect,message):
-	for a in bDic.mul:
-		for b in range(1,27):
-			if cipher == (((expect * a) + b) % 26): 
-				c,d = reversal(a,b)
-				decrypt(c,d,message)
-				print(f"{a},{b}_{c},{d}")
-				
+def decrypt(a, b, message):
+    c, d = reversal(a, b)
+    return encrypt(c, d, message)
+
+
+def reversal(a, b):
+    c = bDic.inverse[a]
+    d = ((26 - b) * c) % 26
+    return c, d
+
+
+# If we know what was the original key, we can find it out using reversal. However, otherwise, we have to brute force
+# the key. To brute force, all we need is magnitude of repetition of characters, and boom. Brute force section below
+def count_chars(text):
+    counts = {}
+    text = ''.join(text.split()).lower()
+    for i in text:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+    return counts
+
+
+def brute_force(cipher, expect, message):
+    for a in bDic.mul:
+        for b in range(1, 27):
+            if cipher == (((expect * a) + b) % 26):
+                c, d = reversal(a, b)
+                print(f"{decrypt(c, d, message)}\n  {a},{b}  _  {c},{d}")
+
+
 def crunch(message):
-	data = countChars(message)
-	data = sorted(data.items(), key=lambda item: item[1],reverse=True)
-	found = False
-	while not found:
-		print(data)
-		C = (input("Please enter Cipher C: ")).lower()
-		for key, val in bDic.dic.items():
-			if val == C:
-				C = key
+    data = count_chars(message)
+    data = sorted(data.items(), key=lambda item: item[1], reverse=True)
+    found = False
+    while not found:
+        print(data)
+        C = (input("Please enter Cipher C: ")).lower()
+        for key, val in bDic.dic.items():
+            if val == C:
+                C = key
+        P = (input("Please enter plain text P: ")).lower()
+        for key, val in bDic.dic.items():
+            if val == P:
+                P = key
+        print("Brute forcing will print 11 possible output for each possible character. Check every entry. Starting now...")
+        time.sleep(3)
+        brute_force(C, P, message)
+        done = input("Is is decoded? Enter Y to end, N to bruteforce again\n-->")
+        if done == 'Y':
+            found = True
 
-		P = (input("Please enter plain text P: ")).lower()
-		for key, val in bDic.dic.items():
-			if val == P:
-				P = key
-		bruteForce(C,P,message)
-# encrypt(15,17,"My name is Huzaifa")
-# bruteForce(9,12,"gliid")
 
-#./d_affine.py encrypt 9,12 "Message"
-#./d_affine.py decrypt 9,12 "Message"
-message = "NTYNC NSOGN XGNGQ NSNSN UIGEX GFNXG NGSMX GTUQZ TGQGF NQCNX SNXGM SFNSO GWKGQ NUCFQ"
-# crunch(message)
-# decrypt(0,0,message)
-# Actually after updating it, it is now ./d_affine.py 
+if len(sys.argv) < 4:
+    print('''Help Menu
+    - Windows
+    python.exe d_affine.py encrypt a,b "Message"  
+    python.exe d_affine.py decrypt a,b "Message"
+    a,b are keys with which the message is encrypted.
+    To Brute Force: python.exe d_affine.py decrypt 0,0 "Message"
+    
+    On linux, replace python.exe with python3
+''')
+    sys.exit(0)
 
-print(encrypt(13,2,"hello"))
+else:
+    action = sys.argv[1]
+    a, b = sys.argv[2].split(",")
+    a, b = int(a), int(b)
+    payload = sys.argv[3]
+    if action == 'encrypt':
+        print(encrypt(a, b, payload))
+    elif action == 'decrypt':
+        if a == 0 and b == 0:
+            crunch(payload)
+        else:
+            c, d = reversal(a, b)
+            print(f"{decrypt(a, b, payload)}\n  {a},{b}  _  {c},{d}")
+    else:
+        print("Please select a valid action")
